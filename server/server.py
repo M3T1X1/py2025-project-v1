276a5f6ebd4f3292f403b10a4a97965fabcf9220
M3T1X1 <dusza-kacper@wp.pl>
2025-05-14 15:37:01 +0200

    dodano szkielety kodu dla client i server, dodano plik .yaml ze wstepna konfiguracja

---
+++ server/server.py
@@ -0,0 +1,9 @@
+class NetworkServer:
+    def __init__(self, port: int):
+        """Inicjalizuje serwer na wskazanym porcie."""
+
+    def start(self) -> None:
+        """Uruchamia nasłuchiwanie połączeń i obsługę klientów."""
+
+    def _handle_client(self, client_socket) -> None:
+        """Odbiera dane, wysyła ACK i wypisuje je na konsolę."""
